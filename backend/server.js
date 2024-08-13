97b675cefa1f2c17214beec6af185e74b2461a05
harsham02 <harshavardhan15015@gmail.com>
2024-08-13 22:35:38 +0530

    second-commit

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 
 const corsOptions = {
-  origin:  process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin:  process.env.FRONTEND_URL || 'https://dealsdray-admin-6zts.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
